cb01c417af5cfa9bbefee47ccd8334457b00344b
BrunoSouza <desouza88@hotmail.com>
2022-10-31 19:01:10 -0300

    Requisito 6: 83% de Coverage

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('', () => {
+    expect(handlerElephants('names')).toEqual('Ilana', 'Orval', 'Bea', 'Jefferson');
+  });
+  it('', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
