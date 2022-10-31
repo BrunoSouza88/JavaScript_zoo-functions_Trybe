74fad34799786ff2f17090ed6c2d29b86beba6c6
BrunoSouza <desouza88@hotmail.com>
2022-10-31 16:02:22 -0300

    Requisito 1 Lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(... parameters) {
-  return data.species.filter(({id}) => parameters.includes(id))
-}
+const getSpeciesByIds = (...parameters) => data.species.filter(({ id }) => parameters.includes(id));
 
 console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
