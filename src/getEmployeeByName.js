c266ab3af086d6e16574aec6c6919362ffd12c4e
BrunoSouza <desouza88@hotmail.com>
2022-10-31 17:03:29 -0300

    Requisito 3. Com lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => (employeeName === undefined ? {} : data.employees
+  .find((element) => element.firstName === employeeName || element.lastName === employeeName));
 
 module.exports = getEmployeeByName;
