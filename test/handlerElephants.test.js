b6a7650ca0fa0a1b4459d0eebf1af56e7c3dcb1a
BrunoSouza <desouza88@hotmail.com>
2022-10-31 19:04:18 -0300

    Preenchimento do que esta sendo testado

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,13 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('', () => {
+  it('Elephants Count', () => {
     expect(handlerElephants('count')).toEqual(4);
   });
-  it('', () => {
+  it('Elephants Names', () => {
     expect(handlerElephants('names')).toEqual('Ilana', 'Orval', 'Bea', 'Jefferson');
   });
-  it('', () => {
+  it('Elephants Age Average', () => {
     expect(handlerElephants('averageAge')).toEqual(10.5);
   });
 });
