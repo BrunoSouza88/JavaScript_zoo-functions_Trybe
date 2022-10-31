9cf52a77a34cc762cd8875937663a3ed4da55f3b
BrunoSouza <desouza88@hotmail.com>
2022-10-31 16:31:58 -0300

    Requisito 1: Refatorando com lint correto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,8 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...parameters) => data.species.filter(({ id }) => parameters.includes(id));
+const getSpeciesByIds = (...parameters) => data.species
+  .filter(({ id }) => parameters
+    .includes(id));
 
 console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
