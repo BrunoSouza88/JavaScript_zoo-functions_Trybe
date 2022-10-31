3d9d15c0891f6d7cf36f165c5628764933a6e74c
BrunoSouza <desouza88@hotmail.com>
2022-10-31 18:25:31 -0300

    Requisito 4. Com lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(() =>
+    id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
+ || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+ || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83');
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return data.employees.filter((element) => element.managers
+      .includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
