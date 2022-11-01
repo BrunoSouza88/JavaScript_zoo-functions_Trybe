e36a6ada611893f50fe3cb8aefe3bb4526174afd
BrunoSouza <desouza88@hotmail.com>
2022-11-01 19:25:30 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animalX = {};
+  if (animal === undefined) {
+    data.species.forEach((element) => {
+      animalX[element.name] = element.residents.length;
+    });
+    return animalX;
+  }
+  if (animal.sex !== undefined) {
+    const nomeAnimal = data.species.find((element) => element.name === animal.specie);
+    const sexAnimal = nomeAnimal.residents.filter((element) => element.sex === animal.sex);
+
+    const tamanho = sexAnimal.length;
+
+    return tamanho;
+  }
+  const nomeAnimal = data.species.find((element) => element.name === animal.specie);
+  const tamanho = nomeAnimal.residents.length;
+  return tamanho;
 }
 
 module.exports = countAnimals;
