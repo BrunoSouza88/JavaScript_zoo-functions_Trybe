40ca3554cf042eb4708edfbd833858c3c336ee35
BrunoSouza <desouza88@hotmail.com>
2022-11-04 17:41:38 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-
+  const specieID = data.employees.find((element) =>
+    id === element.id).responsibleFor[0];
+  const specieX = data.species.find((element) => element.id === specieID).residents;
+  specieX.sort((a, b) => b.age - a.age);
+  return Object.values(specieX[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
